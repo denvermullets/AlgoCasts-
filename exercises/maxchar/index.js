2511baa83330c9b931de2a837599b104d8b444c4
denvermullets <devmullets@gmail.com>
2020-08-24 15:46:16 -0400

    :zap: maxchars, 1 solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+  const characters = {}
+  let max = 0
+  let maxLetter = ''
+
+  // iterate thru string and put each letter into key/value pair
+  // for (let char of str) {
+  //   characters[char] = characters[char] || 1
+  // }
+
+  for (let char of str) {
+    if (characters[char]) {
+      characters[char]++
+    } else {
+      characters[char] = 1
+    }
+  }
+  
+  for (let char in characters) {
+    if (characters[char] > max) {
+      max = characters[char]
+      maxLetter = char
+    }
+  }
+
+  return maxLetter
+
+}
 
 module.exports = maxChar;
