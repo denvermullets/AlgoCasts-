beaff804060dce5f33e77ed6d19229fa973239e0
denvermullets <devmullets@gmail.com>
2020-09-01 07:17:38 -0400

    :zap: capitalize, add 2nd solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -8,11 +8,23 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
+  // my original solution based on hints from video
   let words = []
   for(let word of str.split(' ')) {
     words.push(word[0].toUpperCase() + word.slice(1))
   }
   return words.join(' ')
+
+  // second solution
+  // let result = str[0].toUpperCase()
+  // for (let i = 1; i < str.length; i++) {
+  //   if (str[i - 1] === ' ') {
+  //     result += str[i].toUpperCase()
+  //   } else {
+  //     result += str[i]
+  //   }
+  // }
+  // return result
 }
 
 module.exports = capitalize;
